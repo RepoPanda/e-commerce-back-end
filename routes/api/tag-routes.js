707bd01f7e154c4fe5b0bce1e75d5760e9ea1fed
RepoPanda <124810474+RepoPanda@users.noreply.github.com>
2023-06-10 22:06:21 -0700

    route enabled ability to creat a new tag has been updated

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -17,7 +17,7 @@ router.get('/', async (req, res) => {
 });
 
 // find a single tag by its `id`
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find a single tag by its `id`
   try {
     const tagData = await Tag.findByPk(req.params.id, {
@@ -36,8 +36,14 @@ router.get('/:id', (req, res) => {
   }
 });
 
-router.post('/', (req, res) => {
-  // create a new tag
+// create a new tag
+router.post('/', async (req, res) => {
+  try{
+    const tagData = await Tag.create(req.body);
+    res.status(200).json(tagData);
+  } catch(err) {
+    res.status(400).json(err);
+  }
 });
 
 router.put('/:id', (req, res) => {
