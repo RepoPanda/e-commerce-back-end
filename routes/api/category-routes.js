b4ff1fbd15c6a0bc4bd82034a5db70bc1b829e4e
RepoPanda <124810474+RepoPanda@users.noreply.github.com>
2023-06-10 18:33:30 -0700

    category routes and product routes have been updated

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -51,7 +51,7 @@ router.post('/', async (req, res) => {
 // update a category by its `id` value
 router.put('/:id', async (req, res) => {
   try{
-    const categoryData = await Category.update(req.body,)
+    const categoryData = await Category.update(req.body,{where: {id: req.params.id}})
     res.status(200).json(categoryData);
   } catch(err) {
     res.status(400).json(err);
